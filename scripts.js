5fb57af688374b87ec5dfb8b84f23a8b9297d88d
Felipe <felipelemosaz@gmail.com>
2025-01-14 15:27:20 -0300

    acrescentar div com numero do sorteio

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
 function generateNumber() {
-    const min = Math.ceil(document.querySelector('.input-min').value)
-    const max = Math.floor(document.querySelector('.input-max').value)
-  
-    if (max > min) {
-      const result = Math.floor(Math.random() * (max - min + 1)) + min
-  
-      alert(result)
-    } else {
-      alert('O valor minimo tem que ser MENOR que o valor máximo')
-    }
+  const min = Math.ceil(document.querySelector('.input-min').value);
+  const max = Math.floor(document.querySelector('.input-max').value);
+
+  if (max > min) {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // Exibe o resultado na tela
+    const numberContainer = document.querySelector('.number-container');
+    const numberResult = document.querySelector('.number-result');
+    numberResult.textContent = `Número sorteado: ${result}`;
+    numberContainer.style.display = 'flex';
+
+    // Fecha o contêiner quando o botão for clicado
+    document.querySelector('.close-btn').addEventListener('click', () => {
+      numberContainer.style.display = 'none';
+    });
+  } else {
+    alert('O valor mínimo tem que ser MENOR que o valor máximo');
   }
+}
